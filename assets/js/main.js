6fe45a5eaea430bd6236ba697dc3475f6582bd9f
NadiaQN <quezada.nadia@gmail.com>
2018-02-07 00:35:25 -0300

    agregando estilo night al mapa

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,91 @@ function initMap() {
   // Obtenemos el mapa, centramos la imagen y colocamos zoom
   var map = new google.maps.Map(document.getElementById('map'), {
 	  zoom: 16,
-	  center: laboratoriaChile
+    center: laboratoriaChile,
+    // Agregando estilo nocturno al mapa
+    styles: [
+      { elementType: 'geometry', 
+        stylers: [{ color: '#242f3e' }] },
+      { elementType: 'labels.text.stroke', 
+        stylers: [{ color: '#242f3e' }] },
+      { elementType: 'labels.text.fill', 
+        stylers: [{ color: '#746855' }] },
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{ color: '#263c3f' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#6b9a76' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{ color: '#38414e' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#212a37' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#9ca5b3' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{ color: '#746855' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#1f2835' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#f3d19c' }]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{ color: '#2f3948' }]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{ color: '#17263c' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#515c6d' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{ color: '#17263c' }]
+      }
+    ]
   });
   // Colocamos el marcador para el mapa
   var image = 'https://icon-icons.com/icons2/418/PNG/128/bike_41527.png';
